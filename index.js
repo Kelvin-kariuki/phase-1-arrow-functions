3cd0748da7c22b237bc2629921b81a5875785360
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-08 23:34:44 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+    return (2000/100);
+}
+console.log(divide());
+
+const square = (x) => x **2
+
+console.log(square(2));
+
+const add = (a,b)=> a+b
+
+console.log (add(4+3));
